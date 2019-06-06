c5822074141e80ed78d542d94c69900f273bdab8
Oskar Dudycz <oskar.dudycz@gmail.com>
2019-06-06 09:05:11 +0200

    Updated Benchmark.NET

--- src/MartenBenchmarks/DocumentActions.cs
+++ src/MartenBenchmarks/DocumentActions.cs
@@ -1,14 +1,14 @@
-﻿using System.Linq;
+using System.Linq;
 using BenchmarkDotNet.Attributes;
 using Marten.Testing;
 
 namespace MartenBenchmarks
 {
     [SimpleJob(warmupCount: 2)]
+    [MemoryDiagnoser]
     public class DocumentActions
     {
         public static Target[] Docs = Target.GenerateRandomData(100).ToArray();
-
 
         [GlobalSetup]
         public void Setup()
@@ -17,8 +17,6 @@ namespace MartenBenchmarks
         }
 
         [Benchmark]
-        [MemoryDiagnoser]
-        
         public void InsertDocuments()
         {
             using (var session = BenchmarkStore.Store.OpenSession())
@@ -27,7 +25,5 @@ namespace MartenBenchmarks
                 session.SaveChanges();
             }
         }
-
-
     }
 }
