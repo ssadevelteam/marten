65d4829ab9b8a360f91839c78379ad840225e6c7
jeremydmiller <jeremydmiller@yahoo.com>
2021-01-26 11:28:06 -0600

    New TruncateTable operation for later

--- src/Marten/Events/Daemon/AsyncOptions.cs
+++ src/Marten/Events/Daemon/AsyncOptions.cs
@@ -1,8 +1,38 @@
+using System;
+using System.Collections.Generic;
+using Marten.Internal;
+using Marten.Internal.Operations;
+
 namespace Marten.Events.Daemon
 {
     public class AsyncOptions
     {
         public int BatchSize { get; set; } = 500;
         public int MaximumHopperSize { get; set; } = 2500;
+
+        // TODO -- add an option to just use SQL
+
+        public void DeleteViewTypeOnTeardown<T>()
+        {
+
+        }
+
+        public void DeleteViewTypeOnTeardown(Type type)
+        {
+
+        }
+
+        private readonly IList<Action<IDocumentOperations>> _actions = new List<Action<IDocumentOperations>>();
+
+
+        internal void Teardown(IDocumentOperations operations)
+        {
+            foreach (var action in _actions)
+            {
+                action(operations);
+            }
+        }
     }
+
+
 }
